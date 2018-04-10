bc30b779632e0929732fabb7bade57f2558d40ea
Patrik I <260010@via.dk>
2018-04-10 10:17:43 +0200

    code update

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -6,5 +6,6 @@
 int main(void)
 {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA0);      // turning on led 0 (active low)
+    PORTA = ~(1<<PA3);
+	_delay_ms(500);		   // turning on led 0 (active low)
 }
